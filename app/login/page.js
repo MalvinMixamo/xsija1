3e1c61ed504582e30022a0a39a8690b1c8ac862b
Kipin.Kipon <malvinmixamo@gmail.com>
2026-01-10 17:18:06 +0700

    roarrr

--- app/login/page.js
+++ app/login/page.js
@@ -12,7 +12,7 @@ export default function Login(){
         const data = { nisn, password }
 
         try {
-            const response = await fetch("../api/siswa", {
+            const response = await fetch("/api/siswa", {
                 method: "POST",
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(data)
